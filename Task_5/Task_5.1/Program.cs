e91d2d8a7c315d4f7ffa1547ea9d22295704f2b2
Кирилл Соломатин <kir.solomatin@gmail.com>
2022-03-17 11:16:37 +0300

    add Task 5

---
+++ Task_5/Task_5.1/Program.cs
@@ -0,0 +1,23 @@
+﻿class SeparateMain
+{
+    public static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+        SeparateWordsForString(str);
+        Print(SeparateWordsForString(str));
+        Console.ReadLine();
+    }
+    public static string[] SeparateWordsForString(string text)
+    {
+        string[] words = text.Split(new char[] {' '});
+        return words;
+    }
+    public static void Print(string[] str)
+    {
+        foreach (var item in str)
+        {
+            Console.WriteLine($"{item}");
+        }
+    }
+}
+
---
+++ Task_5/Task_5.2/Program.cs
@@ -0,0 +1,39 @@
+﻿class SeparateMain
+{
+    public static void Main(string[] args)
+    {
+        
+        string str = Console.ReadLine();
+        ReversePhrase(str);
+        Console.ReadLine();
+    }
+    /// <summary>
+    /// Разделение введенного пользователем предложения
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns>Возвращает массив строк.</returns>
+    public static string[] SplitString(string inputString)
+    {
+        string[] words = inputString.Split(new char[] { ' ' });
+        for (int i = 0; i < words.Length; i++)
+        {
+            Console.WriteLine(words[i]);
+        }
+        Console.WriteLine();
+        return words;
+    }
+    public static string[] ReversePhrase(string inputString)
+    {
+        string[] resultString = SplitString(inputString);
+        foreach (var item in resultString)
+        {
+            Array.Reverse(resultString);
+        }
+        foreach (var item in resultString)
+        {
+            Console.WriteLine(item);
+        }
+        
+        return resultString;
+    }
+}
