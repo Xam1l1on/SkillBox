3d2f3f43643017ac45e87225ce2d9aa4ac73d3ee
Кирилл Соломатин <kir.solomatin@gmail.com>
2023-02-19 15:51:08 +0300

    Task_8.1 v 1.0

---
+++ Task_8/Task_8.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Task_8._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> naturalNumber = new List<int>();
+            Random randomNumber = new Random();
+            Program prg= new Program();
+            prg.FillList(naturalNumber, randomNumber);
+            prg.ShowList(naturalNumber);
+            prg.DeleteList(naturalNumber);
+            prg.ShowList(naturalNumber);
+        }
+        public void FillList(List<int> ints, Random randInts) 
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                ints.Add(randInts.Next(0,100));
+            }
+        }
+        public void ShowList(List<int> ints) 
+        {
+            int i = 0;
+            foreach (var item in ints)
+            {
+                Console.WriteLine($"Число {i} = {ints[i]}");
+                i++;
+            }
+            Console.ReadLine();
+        }
+        public void DeleteList(List<int> ints) 
+        {
+            for (int item = ints.Count - 1; item >= 0; item--)
+            {
+                if (ints[item] >= 25 && ints[item] <= 50)
+                {
+                    ints.RemoveAt(item);
+                }
+            }
+        }
+    }
+}
