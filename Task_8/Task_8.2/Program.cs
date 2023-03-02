45627ec296881ded0e8e589ee3760e9a656bcb64
Кирилл Соломатин <kir.solomatin@gmail.com>
2023-03-02 21:43:44 +0300

    Task_8.2 v 1.0

---
+++ Task_8/Task_8.2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Task_8._2
+{
+    internal class Program
+    {
+        Users user;
+        Dictionary<ulong, Users> dicUser = new Dictionary<ulong, Users>();
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            Console.WriteLine($"Добавьте пользователя");
+            ulong nubPhoneUser;
+            pr.AddUser();
+            Console.WriteLine("Найдите нужного пользователя,\n введя номер телефона");
+            nubPhoneUser = ulong.Parse(Console.ReadLine());
+            pr.GetUser(nubPhoneUser);
+            Console.ReadLine();
+        }
+        void AddUser()
+        {
+            ulong numPhone;
+            string lastnameUser;
+            string nameUser;
+            string surnameUser;
+            string checkInputUser = Console.ReadLine();
+            while (!string.IsNullOrEmpty(checkInputUser))
+            {
+                Console.WriteLine($"Введите номер телефона");
+                numPhone = ulong.Parse(Console.ReadLine());
+                Console.WriteLine($"Введите фамилию");
+                lastnameUser = Console.ReadLine();
+                Console.WriteLine($"Введите Имя");
+                nameUser = Console.ReadLine();
+                Console.WriteLine($"Введите Отчество");
+                surnameUser = Console.ReadLine();
+                user = new Users(nameUser, surnameUser, lastnameUser);
+                dicUser.Add(numPhone, user);
+                Console.WriteLine("Продолжить? введите 'да'");
+                checkInputUser = Console.ReadLine();
+            }
+        }
+        void GetUser(ulong id)
+        {
+            foreach (var itemUser in dicUser)
+            {
+                if (dicUser.ContainsKey(id))
+                {
+                    Console.WriteLine($"{itemUser.Key} {itemUser.Value.Lastname} {itemUser.Value.Name} {itemUser.Value.Surname}");
+                }
+                else
+                {
+                    Console.WriteLine("Такого пользователя нет");
+                }
+            }
+        }
+    }
+}
---
+++ Task_8/Task_8.2/Users.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8._2
+{
+    internal struct Users
+    {
+
+        /// <summary>
+        /// Fields
+        /// </summary>
+        #region Fields
+        private string _name;
+        private string _surname;
+        private string _lastname;
+        #endregion
+        /// <summary>
+        /// Properties
+        /// </summary>
+        #region Properties
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Surname { get { return _surname; } set { _surname = value; } }
+        public string Lastname { get { return _lastname; } set { _lastname = value; } }
+        #endregion
+        /// <summary>
+        /// Constructs
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="surname"></param>
+        /// <param name="lastname"></param>
+        #region Constructs
+        public Users(string name, string surname, string lastname)
+        {
+            _name= name;
+            _surname = surname;
+            _lastname = lastname;
+        }
+        #endregion
+        public string PrintUser()
+        {
+            return $"{this.Name} {this.Surname} {this.Lastname}";
+        }
+    }
+}
